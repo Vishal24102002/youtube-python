749f96bd7aaf06c58244b2b363e9a5a55a13112d
Vishal sharma <103731436+Vishal24102002@users.noreply.github.com>
2022-07-25 13:50:00 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,10 @@
+#hello guys we are here to show you the code for factorial without using maths module
+#so without wasting time lets begin now
+a=int(input("enter the number"))
+b=a
+for i in range(1,a,1):
+    a=a*i
+print("the factorial of ",b," is =",a)
+# here we go lets begin you can aslo define another veriable to make your code look more effective 
+#hence firstly we have to test it now lets test it out
+# the code is working properly as required
